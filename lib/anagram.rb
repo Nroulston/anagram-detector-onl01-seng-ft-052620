ae1b1eef60ef07871207903e5ee4c6caad008ff1
Nroulston <nicolas.roulston@gmail.com>
2020-06-05 21:06:49 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require "pry"
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    @sorted_word = @word.chars.sort.join
+    
+    @words_hash = array.each_with_object(Hash.new []) do |word, hash|
+      hash[word.chars.sort.join] += [word]
+    end
+    @matched_word = @words_hash.values_at(@sorted_word)
+    @matched_word[0]
+  end
+    
+end
